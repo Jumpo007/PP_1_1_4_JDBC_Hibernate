f536b4b098d9ebe2f44478739a06268a1264ee3c
Jumpo007 <78562147+Jumpo007@users.noreply.github.com>
2022-07-25 18:31:55 +0300

    com7

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -2,4 +2,8 @@ package jm.task.core.jdbc.util;
 
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "roots";
 }
