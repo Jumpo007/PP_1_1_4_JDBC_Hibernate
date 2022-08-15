a97f6e8a75d36096ec3fee81f814dc2078ee9489
Jumpo007 <78562147+Jumpo007@users.noreply.github.com>
2022-08-15 07:21:45 +0300

    12

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,16 +8,16 @@ import jm.task.core.jdbc.util.Util;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-        UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+        UserDao userDao = new UserDaoHibernateImpl();
         Util.getSessionFactory().openSession();
-        userDaoHibernate.createUsersTable();
-        userDaoHibernate.saveUser("Name1", "LastName1", (byte) 7);
-        userDaoHibernate.saveUser("Name2", "LastName2", (byte) 13);
-        userDaoHibernate.saveUser("Name3", "LastName3", (byte) 19);
-        userDaoHibernate.saveUser("Name4", "LastName4", (byte) 25);
-        System.out.println(userDaoHibernate.getAllUsers());
-        userDaoHibernate.cleanUsersTable();
-        userDaoHibernate.dropUsersTable();
+        userDao.createUsersTable();
+        userDao.saveUser("Name1", "LastName1", (byte) 7);
+        userDao.saveUser("Name2", "LastName2", (byte) 13);
+        userDao.saveUser("Name3", "LastName3", (byte) 19);
+        userDao.saveUser("Name4", "LastName4", (byte) 25);
+        System.out.println(userDao.getAllUsers());
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
         Util.getSessionFactory().close();
 
     }
